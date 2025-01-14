00560816ee90ecab4847c11b8308a09ffcbcd8f0
144droit <indieohren@gmail.com>
2025-01-14 22:47:22 +0100

    fixed style and added main.js

---
+++ RPS/src/main.js
@@ -0,0 +1,9 @@
+const startScreen = document.getElementById('startscreen');
+const startButton = document.getElementById('startbutton');
+const rulesButton = document.getElementById('rulesbutton');
+const score = document.getElementById('score');
+const results = document.getElementById('results');
+
+function startGame() {
+    startScreen.classList.add('hidden');
+}
