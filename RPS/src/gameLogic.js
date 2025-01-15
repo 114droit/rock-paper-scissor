968eb4da5d3f745e603080dd8ec85ddfbb984964
144droit <indieohren@gmail.com>
2025-01-15 16:04:37 +0100

    finished main.js and gameLogic.js

--- RPS/src/gameLogic.js
+++ RPS/src/gameLogic.js
@@ -1,5 +1,37 @@
-const ComputerChoice = ['rock', 'paper', 'scissors'];
+const choices = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-  return ComputerChoice[Math.floor(Math.random() * 3)];
-}
+  return choices[Math.floor(Math.random() * choices.length)];
+};
+
+const gameLogic = {
+  rock: {
+    beats: ['scissors'],
+    loses: ['paper'],
+    ties: ['rock']
+  },
+  paper: {
+    beats: ['rock'],
+    loses: ['scissors'],
+    ties: ['paper']
+  },
+  scissors: {
+    beats: ['paper'],
+    loses: ['rock'],
+    ties: ['scissors']
+  }
+};
+
+function getResult(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return 'tie';
+  }
+
+  if (gameLogic[playerChoice].beats.includes(computerChoice)) {
+    return 'win';
+  }
+
+  return 'lose';
+};
+
+export {getComputerChoice, getResult};
